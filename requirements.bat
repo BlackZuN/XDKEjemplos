e9a6a68792f98989e764a532a26832545cf27fd4
ZuN <rgonzalezc1811@gmail.com>
2018-03-24 10:28:35 -0600

    second version

--- requirements.bat
+++ requirements.bat
@@ -2,4 +2,4 @@
 
 echo python -m pip install --upgrade pip
 echo python -m pip install pyserial==3.4 matplotlib pypiwin32 tweepy
-echo python -m pip install --upgrade pyserial==3.4 matplotlib pypiwin32 tweepy
+echo python -m pip install --upgrade pyserial==3.4 matplotlib pypiwin32 tweepy 
